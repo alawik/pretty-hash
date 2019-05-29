f34ba0a17f2666ed62ca1b2102d4a14b46d3a988
Killian Alawi <alawik@obliquelabs.com>
2019-05-29 02:00:48 -0400

    Create prettyhash.go

---
+++ prettyhash.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	//"os"
+    "reflect"
+    "encoding/hex"
+)
+
+type buf = bytes.Buffer
+
+func isBuffer(b buf) bool {
+    if reflect.TypeOf(b).String() == "bytes.Buffer" {
+        return true
+    } else {
+        return false
+    }
+}
+
+func PrettyHash (b buf) string {
+    if isBuffer(b) && b.Len() > 8 {
+        if b.Len() > 8 {
+            buf := hex.EncodeToString(b.Bytes())
+            return buf[0:6] + ".." + buf[len(buf)-2:]
+        } else {
+            buf := hex.EncodeToString(b.Bytes())
+            return buf
+        }
+    } else {
+        return "Not a hash"
+    }
+}
+
+
+func main() {
+	var b buf
+	b.Write([]byte("Hello World!"))
+
+    fmt.Printf(PrettyHash(b))
+
+}
